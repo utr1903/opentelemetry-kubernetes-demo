906e8a024a62a9cb12317807ed96bc710a891c01
Ugur Turkarslan <52721344+utr1903@users.noreply.github.com>
2024-01-29 21:42:27 +0100

    Implemented cluster provisioning for Azure (#4)
    * Implement baseline deployment
    
    * Add tfplan and lock
    
    * Remove owner
    
    * Implement cluster provisioning
    
    * Implement cluster provisioning

---
+++ infra/cluster/azure/scripts/00_create_baseline_resources.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --project)
+      project="${2}"
+      shift
+      ;;
+    --instance)
+      instance="${2}"
+      shift
+      ;;
+    --location)
+      location="${2}"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+### Check input
+
+# Project
+if [[ $project == "" ]]; then
+  echo -e "Project [--project] is not provided!\n"
+  exit 1
+fi
+
+# Instance
+if [[ $instance == "" ]]; then
+  echo -e "Instance [--instance] is not provided!\n"
+  exit 1
+fi
+
+# Location
+if [[ $location == "" ]]; then
+  location="westeurope"
+  echo -e "Location [--location] is not provided. Using default location ${location}.\n"
+fi
+
+### Set variables
+resourceGroupName="rg${project}base${instance}"
+keyVaultName="kv${project}base${instance}"
+storageAccountName="st${project}base${instance}"
+blobContainerName="${project}tfstates"
+servicePrincipalName="sp${project}${instance}"
+
+# Resource group
+echo "Checking shared resource group [${resourceGroupName}]..."
+resourceGroup=$(az group show \
+  --name $resourceGroupName \
+  2> /dev/null)
+
+if [[ $resourceGroup == "" ]]; then
+  echo " -> Shared resource group does not exist. Creating..."
+
+  resourceGroup=$(az group create \
+    --name $resourceGroupName \
+    --location $location)
+
+  echo -e " -> Shared resource group is created successfully.\n"
+else
+  echo -e " -> Shared resource group already exists.\n"
+fi
+
+# Key vault
+echo "Checking shared key vault [${keyVaultName}]..."
+keyVault=$(az keyvault show \
+  --resource-group $resourceGroupName \
+  --name $keyVaultName \
+  2> /dev/null)
+if [[ $keyVault == "" ]]; then
+  echo " -> Shared key vault does not exist. Creating..."
+
+  keyVault=$(az keyvault create \
+    --resource-group $resourceGroupName \
+    --name $keyVaultName \
+    --location $location)
+
+  echo -e " -> Shared key vault is created successfully.\n"
+else
+  echo -e " -> Shared key vault already exists.\n"
+fi
+
+# Storage account
+echo "Checking shared storage account [${storageAccountName}]..."
+storageAccount=$(az storage account show \
+    --resource-group $resourceGroupName \
+    --name $storageAccountName \
+  2> /dev/null)
+
+if [[ $storageAccount == "" ]]; then
+  echo " -> Shared storage account does not exist. Creating..."
+
+  storageAccount=$(az storage account create \
+    --resource-group $resourceGroupName \
+    --name $storageAccountName \
+    --sku "Standard_LRS" \
+    --allow-blob-public-access true \
+    --encryption-services "blob")
+
+  echo -e " -> Shared storage account is created successfully.\n"
+else
+  echo -e " -> Shared storage account already exists.\n"
+fi
+
+# Terraform blob container
+echo "Checking Terraform blob container [${blobContainerName}]..."
+terraformBlobContainer=$(az storage container show \
+  --account-name $storageAccountName \
+  --name $blobContainerName \
+  2> /dev/null)
+
+if [[ $terraformBlobContainer == "" ]]; then
+  echo " -> Terraform blob container does not exist. Creating..."
+
+  terraformBlobContainer=$(az storage container create \
+    --account-name $storageAccountName \
+    --name $blobContainerName \
+    2> /dev/null)
+
+  echo -e " -> Terraform blob container is created successfully.\n"
+else
+  echo -e " -> Terraform blob container already exists.\n"
+fi
+
+# Service principal
+echo "Checking service principal [${servicePrincipalName}]..."
+subscriptionId=$(az account show | jq -r .id)
+
+servicePrincipal=$(az ad sp show \
+  --id $servicePrincipalName \
+  2> /dev/null)
+if [[ $servicePrincipal == "" ]]; then
+  echo " -> Service principal does not exist. Creating..."
+
+  servicePrincipal=$(az ad sp create-for-rbac \
+    --name $servicePrincipalName \
+    --role owner \
+    --scopes "/subscriptions/${subscriptionId}" \
+    --output json)
+
+  echo -e " -> Service principal is created successfully.\n"
+
+  servicePrincipalAppId=$(echo $servicePrincipal | jq -r .appId)
+  servicePrincipalSecret=$(echo $servicePrincipal | jq -r .password)
+
+  # Store service principal credentials in key vault
+  echo -e " -> Storing service principal credentials into key vault."
+  az keyvault secret set \
+    --vault-name $keyVaultName \
+    --name "${servicePrincipalName}-appid" \
+    --value $servicePrincipalAppId \
+    > /dev/null
+
+  az keyvault secret set \
+    --vault-name $keyVaultName \
+    --name "${servicePrincipalName}-secret" \
+    --value $servicePrincipalSecret \
+    > /dev/null
+
+  echo -e " -> Service principal credentials are stored successfully.\n"
+else
+  echo -e " -> Service principal already exists.\n"
+fi
+
+
---
+++ infra/cluster/azure/scripts/01_deploy_cluster.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --project)
+      project="${2}"
+      shift
+      ;;
+    --instance)
+      instance="${2}"
+      shift
+      ;;
+    --location)
+      location="${2}"
+      shift
+      ;;
+    --k8s-version)
+      k8sVersion="${2}"
+      shift
+      ;;
+    --destroy)
+      flagDestroy="true"
+      shift
+      ;;
+    --dry-run)
+      flagDryRun="true"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+### Check input
+
+# Project
+if [[ $project == "" ]]; then
+  echo -e "Project [--project] is not provided!\n"
+  exit 1
+fi
+
+# Instance
+if [[ $instance == "" ]]; then
+  echo -e "Instance [--instance] is not provided!\n"
+  exit 1
+fi
+
+# Location
+if [[ $location == "" ]]; then
+  location="westeurope"
+  echo -e "Location [--location] is not provided. Using default location ${location}.\n"
+fi
+
+# K8s version
+if [[ $k8sVersion == "" ]]; then
+  k8sVersion="1.27.1"
+  echo -e "K8s version [--k8s-version] is not provided. Using default version ${k8sVersion}.\n"
+fi
+
+### Set variables
+
+# Base
+baseResourceGroupName="rg${project}base${instance}"
+baseKeyVaultName="kv${project}base${instance}"
+baseStorageAccountName="st${project}base${instance}"
+baseBlobContainerName="${project}tfstates"
+
+# Main
+mainResourceGroupName="rg${project}main${instance}"
+mainAksResourceName="aks${project}main${instance}"
+mainAksNodepoolResourceGroupName="rgaks${project}main${instance}"
+
+### Perform Terraform deployment
+azureAccount=$(az account show)
+tenantId=$(echo $azureAccount | jq -r .tenantId)
+subscriptionId=$(echo $azureAccount | jq -r .id)
+
+if [[ $flagDestroy != "true" ]]; then
+
+  # Initialize Terraform
+  terraform -chdir=../terraform init \
+    -upgrade \
+    -backend-config="tenant_id=${tenantId}" \
+    -backend-config="subscription_id=${subscriptionId}" \
+    -backend-config="resource_group_name=${baseResourceGroupName}" \
+    -backend-config="storage_account_name=${baseStorageAccountName}" \
+    -backend-config="container_name=${baseBlobContainerName}" \
+    -backend-config="key=cluster.tfstate"
+
+  # Plan Terraform
+  terraform -chdir=../terraform plan \
+    -var aks_resource_group_name=$mainResourceGroupName \
+    -var aks_resource_name=$mainAksResourceName \
+    -var aks_nodepool_resource_name=$mainAksNodepoolResourceGroupName \
+    -var aks_version=$k8sVersion \
+    -var location=$location \
+    -out "./tfplan"
+
+    if [[ $flagDryRun != "true" ]]; then
+    
+      # Apply Terraform
+      terraform -chdir=../terraform apply tfplan
+
+      # Get AKS credentials
+      az aks get-credentials \
+        --resource-group $mainResourceGroupName \
+        --name $aksResourceName \
+        --overwrite-existing
+    fi
+else
+
+  # Destroy resources
+  terraform -chdir=../terraform destroy \
+    -var aks_resource_group_name=$mainResourceGroupName \
+    -var aks_resource_name=$mainAksResourceName \
+    -var aks_nodepool_resource_name=$mainAksNodepoolResourceGroupName \
+    -var aks_version=$k8sVersion \
+    -var location=$location
+fi
