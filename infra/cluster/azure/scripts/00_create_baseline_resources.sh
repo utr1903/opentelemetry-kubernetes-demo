c9271de45592c78021bbabd4bca6fa15c982faa1
uturkarslan <ugurturkarslan@gmail.com>
2024-01-30 08:30:18 +0100

    Implement service principal creation

--- infra/cluster/azure/scripts/00_create_baseline_resources.sh
+++ infra/cluster/azure/scripts/00_create_baseline_resources.sh
@@ -43,57 +43,36 @@ fi
 
 ### Set variables
 resourceGroupName="rg${project}base${instance}"
-keyVaultName="kv${project}base${instance}"
 storageAccountName="st${project}base${instance}"
 blobContainerName="${project}tfstates"
-servicePrincipalName="sp${project}${instance}"
 
 # Resource group
-echo "Checking shared resource group [${resourceGroupName}]..."
+echo "Checking base resource group [${resourceGroupName}]..."
 resourceGroup=$(az group show \
   --name $resourceGroupName \
   2> /dev/null)
 
 if [[ $resourceGroup == "" ]]; then
-  echo " -> Shared resource group does not exist. Creating..."
+  echo " -> Base resource group does not exist. Creating..."
 
   resourceGroup=$(az group create \
     --name $resourceGroupName \
     --location $location)
 
-  echo -e " -> Shared resource group is created successfully.\n"
+  echo -e " -> Base resource group is created successfully.\n"
 else
-  echo -e " -> Shared resource group already exists.\n"
-fi
-
-# Key vault
-echo "Checking shared key vault [${keyVaultName}]..."
-keyVault=$(az keyvault show \
-  --resource-group $resourceGroupName \
-  --name $keyVaultName \
-  2> /dev/null)
-if [[ $keyVault == "" ]]; then
-  echo " -> Shared key vault does not exist. Creating..."
-
-  keyVault=$(az keyvault create \
-    --resource-group $resourceGroupName \
-    --name $keyVaultName \
-    --location $location)
-
-  echo -e " -> Shared key vault is created successfully.\n"
-else
-  echo -e " -> Shared key vault already exists.\n"
+  echo -e " -> Base resource group already exists.\n"
 fi
 
 # Storage account
-echo "Checking shared storage account [${storageAccountName}]..."
+echo "Checking base storage account [${storageAccountName}]..."
 storageAccount=$(az storage account show \
     --resource-group $resourceGroupName \
     --name $storageAccountName \
   2> /dev/null)
 
 if [[ $storageAccount == "" ]]; then
-  echo " -> Shared storage account does not exist. Creating..."
+  echo " -> Base storage account does not exist. Creating..."
 
   storageAccount=$(az storage account create \
     --resource-group $resourceGroupName \
@@ -102,9 +81,9 @@ if [[ $storageAccount == "" ]]; then
     --allow-blob-public-access true \
     --encryption-services "blob")
 
-  echo -e " -> Shared storage account is created successfully.\n"
+  echo -e " -> Base storage account is created successfully.\n"
 else
-  echo -e " -> Shared storage account already exists.\n"
+  echo -e " -> Base storage account already exists.\n"
 fi
 
 # Terraform blob container
@@ -126,45 +105,3 @@ if [[ $terraformBlobContainer == "" ]]; then
 else
   echo -e " -> Terraform blob container already exists.\n"
 fi
-
-# Service principal
-echo "Checking service principal [${servicePrincipalName}]..."
-subscriptionId=$(az account show | jq -r .id)
-
-servicePrincipal=$(az ad sp show \
-  --id $servicePrincipalName \
-  2> /dev/null)
-if [[ $servicePrincipal == "" ]]; then
-  echo " -> Service principal does not exist. Creating..."
-
-  servicePrincipal=$(az ad sp create-for-rbac \
-    --name $servicePrincipalName \
-    --role owner \
-    --scopes "/subscriptions/${subscriptionId}" \
-    --output json)
-
-  echo -e " -> Service principal is created successfully.\n"
-
-  servicePrincipalAppId=$(echo $servicePrincipal | jq -r .appId)
-  servicePrincipalSecret=$(echo $servicePrincipal | jq -r .password)
-
-  # Store service principal credentials in key vault
-  echo -e " -> Storing service principal credentials into key vault."
-  az keyvault secret set \
-    --vault-name $keyVaultName \
-    --name "${servicePrincipalName}-appid" \
-    --value $servicePrincipalAppId \
-    > /dev/null
-
-  az keyvault secret set \
-    --vault-name $keyVaultName \
-    --name "${servicePrincipalName}-secret" \
-    --value $servicePrincipalSecret \
-    > /dev/null
-
-  echo -e " -> Service principal credentials are stored successfully.\n"
-else
-  echo -e " -> Service principal already exists.\n"
-fi
-
-
