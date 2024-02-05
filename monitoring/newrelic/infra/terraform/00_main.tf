888cf14be3fdc9b41de0c7b7854857f2951b3a7f
uturkarslan <ugurturkarslan@gmail.com>
2024-02-05 11:57:57 +0100

    Upgrade nrotelk8s Terraform module to 0.3.0.

--- monitoring/newrelic/infra/terraform/00_main.tf
+++ monitoring/newrelic/infra/terraform/00_main.tf
@@ -4,7 +4,7 @@
 
 # Kubernetes infra monitoring
 module "nrotelk8s" {
-  source = "github.com/newrelic-experimental/monitoring-kubernetes-with-opentelemetry.git?ref=newrelic-monitoring-0.2.0/monitoring/terraform"
+  source = "github.com/newrelic-experimental/monitoring-kubernetes-with-opentelemetry.git?ref=newrelic-monitoring-0.3.0/monitoring/terraform"
 
   NEW_RELIC_ACCOUNT_ID = var.NEW_RELIC_ACCOUNT_ID
   NEW_RELIC_API_KEY    = var.NEW_RELIC_API_KEY
